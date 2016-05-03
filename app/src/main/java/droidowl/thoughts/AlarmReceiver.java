8fa04cbb73e6ab5fecb106345d673bf342dc32a0
Spencer Amann <amanns@mail.gvsu.edu>
2016-05-03 12:57:14 -0400

    Annotate receiver

--- app/src/main/java/droidowl/thoughts/AlarmReceiver.java
+++ app/src/main/java/droidowl/thoughts/AlarmReceiver.java
@@ -7,7 +7,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
+import org.androidannotations.annotations.EReceiver;
 import org.androidannotations.annotations.sharedpreferences.Pref;
 
 import java.util.Calendar;
@@ -16,6 +18,7 @@ import java.util.GregorianCalendar;
 /**
  * Created by droidowl on 4/21/16.
  */
+@EReceiver
 public class AlarmReceiver extends BroadcastReceiver {
 
     @Pref
@@ -24,12 +27,14 @@ public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Calendar now = GregorianCalendar.getInstance();
-        int dayOfWeek = now.get(Calendar.DATE);
         int hourNow = now.get(Calendar.HOUR) * 3600000;
         int minuteNow = now.get(Calendar.MINUTE) * 60000;
         if(mPrefs.notificationsEnabled().get() &&
                 hourNow == mPrefs.notificationHour().get() &&
                 minuteNow == mPrefs.notificationMinute().get()) {
+            Log.e("TIME TO SEND", String.valueOf(hourNow) + ":" + String
+                    .valueOf
+                    (minuteNow));
             NotificationCompat.Builder mBuilder =
                     new NotificationCompat.Builder(context)
                             .setContentTitle(context.getString(R.string.notification_title))
@@ -44,5 +49,8 @@ public class AlarmReceiver extends BroadcastReceiver {
             NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
             mNotificationManager.notify(1, mBuilder.build());
         }
+        Log.e("TIME NOT TO SEND", String.valueOf(hourNow) + ":" + String
+                .valueOf
+                (minuteNow));
     }
 }
